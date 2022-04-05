0fc2afbb53c7b77393dfe9f8093961f1d33d8f02
Hyun Sung Cho <hcho774@gmail.com>
2022-04-05 17:08:54 -0400

    commit tree c4f3cbacb54dd87e36cbfd8dd6bb99f27467cc37

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
